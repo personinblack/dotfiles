86216ae1260ff534f176f0426dcd94918422c6eb
personinblack <berkay@tuta.io>
2019-12-19 18:02:58 +0300

    dwm/autostart.sh: leave a space at the start of xsetroot

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -6,6 +6,6 @@ feh --bg-max ~/.wp &
 
 while :
 do
-    xsetroot -name "$(date "+%a %d %b %Y %R %S") :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
+    xsetroot -name " $(date "+%a %d %b %Y %R %S") :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
     sleep 1
 done
