3a7d2a94d073eeb55c07fb45e561022d42e43268
personinblack <berkay@tuta.io>
2019-12-19 06:39:07 +0300

    dwm/config.h: increase gapps 5 -> 10

--- dwm/aur/dwm/config.h
+++ dwm/aur/dwm/config.h
@@ -13,7 +13,7 @@ static const char *screenshot_select[] = { "/usr/bin/scrot", "-e", "mv $f ~/scre
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
