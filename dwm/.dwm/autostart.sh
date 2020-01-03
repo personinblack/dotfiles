3b4f5ad78062ae05ead076dc4e2bacd9edb6f272
personinblack <berkay@tuta.io>
2020-01-04 01:21:44 +0300

    dwm/autostart.sh: add the missing semicolon before the second

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+picom --experimental-backends -b &
 discord &
 firefox &
 feh --bg-max ~/.wp &
 
 while :
 do
-    xsetroot -name " $(date "+%a %d %b %Y %R %S") :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
+    xsetroot -name " $(date "+%a %d %b %Y %R:%S") :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
     sleep 1
 done
