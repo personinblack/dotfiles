0d05760459f84d6f623ff9f4a81add9f3c7d902e
personinblack <berkay@tuta.io>
2020-03-19 22:26:30 +0300

    dwm/config.h: stop pixel snapping

--- dwm/aur/dwm/config.h
+++ dwm/aur/dwm/config.h
@@ -14,7 +14,7 @@ static const char *screenshot_select[] = { "/usr/bin/scrot", "-e", "mv $f ~/scre
 /* appearance */
 static const unsigned int borderpx      = 1;        /* border pixel of windows */
 static const unsigned int gappx         = 10;        /* gaps between windows */
-static const unsigned int snap          = 32;       /* snap pixel */
+static const unsigned int snap          = 1;       /* snap pixel */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 1;        /* 0 means bottom bar */
 static const char *fonts[]              = { "scientifica:style=Bold:size=13", "Noto Color Emoji:style=Regular:size=12" };
