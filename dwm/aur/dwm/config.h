44a0ad7ef1a129d350f32ddd53099a8d8b407b22
personinblack <berkay@tuta.io>
2020-09-22 22:56:09 +0300

    update dwm font sizes according to dpi and replace Noto Color Emoji with Symbola

--- dwm/aur/dwm/config.h
+++ dwm/aur/dwm/config.h
@@ -17,8 +17,8 @@ static const unsigned int gappx         = 40;        /* gaps between windows */
 static const unsigned int snap          = 1;       /* snap pixel */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 1;        /* 0 means bottom bar */
-static const char *fonts[]              = { "scientifica:size=8:style=Bold", "Noto Color Emoji:style=Regular:size=12" };
-static const char dmenufont[]           = "scientifica:size=8:style=Bold";
+static const char *fonts[]              = { "scientifica:size=6:style=Bold", "Symbola:style=Regular:size=6" };
+static const char dmenufont[]           = "scientifica:size=6:style=Bold";
 static const unsigned int baralpha      = OPAQUE;
 static const unsigned int borderalpha   = OPAQUE;
 static const char col_selbg[]           = "#0D0E0E";
