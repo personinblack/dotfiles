40d3f516aa91866646621bfb5d0de26af0c6729f
personinblack <berkay@tuta.io>
2020-12-14 04:13:27 +0300

    st: update config defaults

--- st/aur/st/config.h
+++ st/aur/st/config.h
@@ -74,6 +74,14 @@ static unsigned int cursorthickness = 2;
  */
 static int bellvolume = 100;
 
+/* visual-bell timeout in ms (0 to disable visual-bell) */
+static int vbelltimeout = 1500;
+
+/* choose predefined visual-bell cells to inverse, or define your own logic */
+// #define VBCELL x==0 || x==right || y==0 || y==bottom  /* border */
+// #define VBCELL 1  /* all cells - whole screen */
+#define VBCELL y==bottom && x>right-2  /* bottom-right */
+
 /* default TERM value */
 char *termname = "st-256color";
 
