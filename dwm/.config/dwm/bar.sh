893c36080641bf750d0a065cf5df3b95756e4e93
personinblack <berkay@tuta.io>
2021-04-25 18:56:31 +0300

    dwm/bar: hide volume level when muted

--- dwm/.config/dwm/bar.sh
+++ dwm/.config/dwm/bar.sh
@@ -1,1 +1,7 @@
-xsetroot -name " $(date "+%a %d %b %Y %R:%S") :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
+MUTE=$(pactl list sinks | awk '/^\sMute/ {print $2}')
+VOLUME=" :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
+if [ $MUTE == "yes" ]; then
+  VOLUME=""
+fi
+
+xsetroot -name " $(date "+%a %d %b %Y %R:%S")$VOLUME"
