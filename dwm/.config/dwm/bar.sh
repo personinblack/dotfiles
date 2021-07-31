32d49d941ff1d3802a736d5e9f16e0084e2c0fb7
personinblack <berkay@tuta.io>
2021-08-01 00:38:20 +0300

    dwm: refactor bar.sh and add user/hostname && temperature

--- dwm/.config/dwm/bar.sh
+++ dwm/.config/dwm/bar.sh
@@ -1,7 +1,37 @@
-MUTE=$(pactl list sinks | awk '/^\sMute/ {print $2}')
-VOLUME=" :: $(pactl list sinks | awk '/^\sVolume/ {print $5}')"
-if [ $MUTE == "yes" ]; then
-  VOLUME=""
-fi
+SEPERATOR=" ▓"
+BAR="$USER@$(cat /etc/hostname)$SEPERATOR"
 
-xsetroot -name " $(date "+%a %d %b %Y %R:%S")$VOLUME"
+mod_volume() {
+  mute=$(pactl list sinks | awk '/^\sMute/ {print $2}')
+  if [[ $mute == "yes" ]]; then
+    echo ""
+  else
+    echo $(pactl list sinks | awk '/^\sVolume/ {print $5}')
+  fi
+}
+
+mod_temp() {
+  temp=$(sensors acpitz-acpi-0 | awk '{ if(NR==5) { print($2++) } }')
+  if (( "$temp" >= 70 )); then
+    echo "$temp°C"
+  else
+    echo ""
+  fi
+}
+
+mod_date() {
+  echo $(date "+%a %d %b %Y %R:%S")
+}
+
+MODS=($(mod_temp) "$(mod_date)" $(mod_volume))
+
+for mod in "${MODS[@]}"; do
+  if [[ $mod == "" ]]; then
+    continue
+  fi
+
+  BAR+="$mod"
+  BAR+=$SEPERATOR
+done
+
+xsetroot -name "${BAR%$SEPERATOR*}"
