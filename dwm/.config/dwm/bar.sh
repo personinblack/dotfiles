e5362273f7c591184d11decc074ef0b1c86b435a
personinblack <berkay@tuta.io>
2023-06-21 04:04:56 +0300

    dwm: rework && fix bar volume being n/a when inactive
    no more unnecessary awk. fish wasn't auto-completing "pactl get-*" commands,
    and i wasn't aware of their existence until i looked at the man page.
    which is why i was using "pactl list" && awk.

--- dwm/.config/dwm/bar.sh
+++ dwm/.config/dwm/bar.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-SEPERATOR=" }~{ "
-BAR=""
-
 mod_volume() {
-  mute=$(pactl list sinks | awk '/^\sMute/ {print $2}')
-  if [[ $mute == "yes" ]]; then
-    VOL="NA%"
-  else
-    VOL="$(pactl list sinks | awk '
-      /^\sState/ {
-        State = $2
-      }
-      /^\sName/ {
-        if ($2 == "easyeffects_sink")
-          State = "NOT"
-      }
-      /^\sMute/ {
-        Muted = $2
-      }
+  SINK="$(pactl get-default-sink)"
+  VOL="$(printf "%03d%%" $(pactl get-sink-volume $SINK | cut -s -d"/" -f2))"
 
-      /^\sVolume/ {
-        if (State == "RUNNING")
-          if (Muted == "no")
-            print $5 " "
-          else
-            print "NA% "
-      }
-    ')"
-  fi
-
-  if [[ $VOL == "" ]]; then
-    VOL="NA%"
+  if [[ $VOL == "" || $(pactl get-sink-mute $SINK) == "Mute: yes" ]]; then
+    VOL="N/A%"
   fi
 
   echo $VOL
