ba8f4499b9990eda114e8338df849b4e4fa67419
personinblack <berkay@tuta.io>
2023-06-30 09:45:23 +0300

    vim: floating terminal with mod + t

--- nvim/.config/nvim/after/plugin/toggleterm.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -5,9 +5,14 @@ require("toggleterm").setup({
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
     cmd = "lazygit",
-    direction = "float",
+    float_opts = { border = "curved" },
+})
+local fish = Terminal:new({
+    cmd = "fish",
     float_opts = { border = "curved" },
 })
 
 vim.keymap.set("n", "<Leader>g", function() lazygit:toggle() end,
+    { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>t", function() fish:toggle() end,
     { noremap = true, silent = true })
