e63d40927fbaace2ccc4a574b1ab9e0659e4b9d0
personinblack <berkay@tuta.io>
2023-10-07 19:05:37 +0300

    vim: telescope lsp symbols map

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,11 @@ local builtin = require("telescope.builtin")
 local actions = require("telescope.actions")
 
 vim.keymap.set("n", "<leader>s", builtin.find_files, {})
+vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>sa", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ss", builtin.lsp_workspace_symbols, {})
 
 require("telescope").setup {
     defaults = {
